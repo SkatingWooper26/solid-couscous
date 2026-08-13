1b3737c897b445aa31c1b400d60e65c782e47f83
SkatingWooper26 <SkatingWooper26@gmail.com>
2026-08-13 20:45:11 +0100

    Completed project 8

--- Python/Basics/CFConverter.py
+++ Python/Basics/CFConverter.py
@@ -7,11 +7,13 @@ def fahrenheitToCelsius(fahrenheit):
 
 choice = input("Do you want to convert CELSIUS or FAHRENHEIT? (C/F): ").strip().lower()
 
-if hoice == "c":
+if choice == "c":
     celsius = float(input("Enter temperature in Celsius: "))
     fahrenheit = celsiusToFahrenheit(celsius)
     print(f"{celsius}°C is equal to {fahrenheit}°F")
-else:
+elif choice == "f":
     fahrenheit = float(input("Enter temperature in Fahrenheit: "))
     celsius = fahrenheitToCelsius(fahrenheit)
     print(f"{fahrenheit}°F is equal to {celsius}°C")
+else:
+    print("Invalid choice. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
