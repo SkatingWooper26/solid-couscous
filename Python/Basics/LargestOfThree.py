8299818cbeacd25a6b211a7819e997e23e21fe54
SkatingWooper26 <SkatingWooper26@gmail.com>
2026-08-14 20:57:37 +0100

    Project 12 added and complete

---
+++ Python/Basics/LargestOfThree.py
@@ -0,0 +1,13 @@
+
+def largestOfThree(a, b, c):
+    if a == b == c:
+        return None
+    return max(a, b, c)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+biggestNum = largestOfThree(num1, num2, num3)
+
+print(f"The biggest of these numbers was {biggestNum}")
