0d88c650656c3efd5877aa4501e3cc23c613bbb9
SkatingWooper26 <SkatingWooper26@gmail.com>
2026-08-21 11:30:41 +0100

    Updated project 28

--- Python/Basics/28_MergeDictionarys.py
+++ Python/Basics/28_MergeDictionarys.py
@@ -3,7 +3,7 @@
 def combine_users(*users):
     return {
         f"user_{user_num}": user
-        for user_num, user in enumerate(users)
+        for user_num, user in enumerate(users, start=1)
     }
 
 def ask_info(questions):
@@ -19,15 +19,18 @@ def ask_info(questions):
     return answers
                 
 def main():
-    question_list = {"name" : str, 
-                     "age" : int, 
-                     "city" : str}
-    user_0 = ask_info(question_list)
-    print("Now for the next person")
+    question_list = {"name": str,
+                     "age": int,
+                     "city": str}
     user_1 = ask_info(question_list)
-    combined_dict = combine_users(user_0, user_1)
+    print("Now for the next person")
+    user_2 = ask_info(question_list)
+    combined_dict = combine_users(user_1, user_2)
     print("The combined data is:")
     print(combined_dict)
+    print("The separate data is:")
+    for user in combined_dict.values():
+        print(user)
     
 
 if __name__ == "__main__":
