a7226cec5032219b227678fce22e1b2cc3c1c965
SkatingWooper26 <SkatingWooper26@gmail.com>
2026-09-19 00:03:32 +0100

    Basic outline for tkinter calculator

---
+++ Simple Projects/TKCalculator.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+expr = ""
+
+def add(symbol: str) -> None:
+    global expr, displayed_text
+    
+    expr += symbol
+    displayed_text.set(expr)
+    
+def translate_expr() -> str:
+    global expr
+    
+    table = str.maketrans("×÷", "*/")
+    return expr.translate(table)
+    
+def equal() -> None:
+    global expr, displayed_text
+    try:
+        answer = str(eval(translate_expr()))
+        clear()
+        displayed_text.set(answer)
+        expr = answer
+    except:
+        clear()
+        displayed_text.set("There was an error.")
+    
+def clear() -> None:
+    global expr
+    
+    expr = ""
+    
+def main() -> None:
+    global expr, displayed_text
+    
+    root = tk.Tk()
+    root.title("Calculator")
+    root.geometry("250x300")
+    
+    displayed_text = tk.StringVar()
+    displayed_text.set(expr)
+    
+    display = tk.Label(textvariable=displayed_text).grid(columnspan=4, row = 0)
+    
+    buttons = (("7", 0, 1), ("8", 1, 1), ("9", 2, 1), ("+", 3, 1),
+               ("4", 0, 2), ("5", 1, 2), ("6", 2, 2), ("-", 3, 2),
+               ("1", 0, 3), ("2", 1, 3), ("3", 2, 3), ("×", 3, 3),
+               ("C", 0, 4), ("0", 1, 4), ("=", 2, 4), ("÷", 3, 4))
+    
+    for sym, x, y in buttons:
+        tk.Button(root, text=sym, width = 3, height = 2, font = ("Fixedsys", 10, "bold"),
+                  command = lambda symbol = sym: add(symbol) if symbol not in ("=", "C") else clear() if symbol == "C" else equal()
+                  ).grid(column = x, row = y, padx = 5, pady = 5)
+        
+    root.mainloop()
+    
+if __name__ == "__main__":
+    main()
